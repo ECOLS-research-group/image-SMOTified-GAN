8d4009f898e049a6e19eb5a9448f17c5b2895ba3
Deshant Singh <89639206+deshantDEV29@users.noreply.github.com>
2023-08-14 23:40:23 +1200

    Update on GAN
    Not working yet

--- GAN2.py
+++ GAN2.py
@@ -1,60 +1,109 @@
 import os
 import numpy as np
-import cv2
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense, Reshape, LeakyReLU, BatchNormalization, Conv2DTranspose, Conv2D
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
-from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers, models
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-# Define paths
-dir_path = 'C:\\Users\\singh\\OneDrive\\Documents\\Projects\\SMOTified-GAN-Image\\data\\flowers\\train'
-new_dir_path = 'C:\\Users\\singh\\OneDrive\\Documents\\Projects\\SMOTified-GAN-Image\\data\\flowers\\generatedByGAN'
+# Define the Generator model
+def build_generator(latent_dim, channels):
+    model = models.Sequential()
+    model.add(layers.Dense(128 * 128 * channels, input_dim=latent_dim))
+    model.add(layers.Reshape((128, 128, channels)))
+    model.add(layers.Conv2DTranspose(128, (4, 4), strides=(2, 2), padding='same'))
+    model.add(layers.Conv2DTranspose(64, (4, 4), strides=(2, 2), padding='same'))
+    model.add(layers.Conv2DTranspose(channels, (4, 4), activation='sigmoid', padding='same'))  # Updated here
+    return model
 
-# Step 1: Load and preprocess the image data
-image_size = (128, 128)  # Adjust image size as needed
-datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+# Define the Discriminator model
+def build_discriminator(img_shape):
+    model = models.Sequential()
+    model.add(layers.Conv2D(64, (4, 4), strides=(2, 2), padding='same', input_shape=img_shape))
+    model.add(layers.Conv2D(128, (4, 4), strides=(2, 2), padding='same'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1, activation='sigmoid'))
+    return model
+
+
+# Define GAN model
+def build_gan(generator, discriminator):
+    discriminator.trainable = False
+    model = models.Sequential()
+
+    # Add generator layers
+    model.add(generator)
+
+    # Add discriminator layers (without the fully connected layer)
+    for layer in discriminator.layers[:-1]:  # Exclude the last layer (fully connected)
+        model.add(layer)
+
+    return model
+
+# Parameters
+latent_dim = 100
+img_shape = (128, 128, 3)  # Update this to match your dataset's image size
+channels = img_shape[-1]
+num_categories = 2
+
+# Build and compile the models
+generator = build_generator(latent_dim, channels)
+discriminator = build_discriminator((128, 128, 3))
+discriminator.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(lr=0.0002, beta_1=0.5))  # Compile discriminator
+gan = build_gan(generator, discriminator)
+gan.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(lr=0.0002, beta_1=0.5))
+
+# Load and preprocess the dataset using ImageDataGenerator
+datagen = ImageDataGenerator(rescale=1.0 / 255)
+data_dir = 'C:\\Users\\singh\\OneDrive\\Documents\\Projects\\SMOTified-GAN-Image\\data\\xray\\train'
+batch_size = 32  # Adjust the batch size to be a multiple of the number of categories (e.g., 32/2 = 16)
+image_size = (128, 128)
+
 train_generator = datagen.flow_from_directory(
-    dir_path,
+    data_dir,
     target_size=image_size,
-    batch_size=32,
-    class_mode='input',  # Use 'input' for loading images without changing their format
+    batch_size=batch_size,
+    class_mode='categorical',
     shuffle=True
 )
 
-# Convert the images to numpy arrays
-images = []
-for _ in range(len(train_generator)):
-    batch_images, _ = train_generator.next()
-    images.append(batch_images)
 
-images = np.concatenate(images)
+# Create a directory to save generated images
+output_dir = 'C:\\Users\\singh\\OneDrive\\Documents\\Projects\\SMOTified-GAN-Image\\data\\xray\\generated'
+os.makedirs(output_dir, exist_ok=True)
 
-# Step 2: Define the generator network
-latent_dim = 100
-generator_input = Input(shape=(latent_dim,))
-generator_model = Dense(128 * 16 * 16, activation='relu')(generator_input)
-generator_model = BatchNormalization()(generator_model)
-generator_model = Reshape((16, 16, 128))(generator_model)
-generator_model = Conv2DTranspose(64, kernel_size=4, strides=2, padding='same', activation='relu')(generator_model)
-generator_model = BatchNormalization()(generator_model)
-generator_output = Conv2DTranspose(3, kernel_size=4, strides=2, padding='same', activation='sigmoid')(generator_model)
-generator = Model(inputs=generator_input, outputs=generator_output)
+# Training loop
+epochs = 5000
+steps_per_epoch = len(train_generator)
 
-# Step 3: Compile the generator
-generator.compile(optimizer=Adam(0.0002, 0.5), loss='binary_crossentropy')
+for epoch in range(epochs):
+    for step in range(steps_per_epoch):
+        real_images, real_labels = train_generator.next()  # Get both images and labels from the generator
 
-# Step 4: Generate synthetic images using the GAN
-num_samples = len(images)
-num_synthetic_samples = num_samples  # Adjust the number of synthetic samples as needed
+        noise = np.random.normal(0, 1, (batch_size, latent_dim))
+        generated_images = generator.predict(noise)
 
-z = np.random.normal(size=(num_synthetic_samples, latent_dim))
-synthetic_images = generator.predict(z)
+        real_labels = np.ones(batch_size)
+        fake_labels = np.zeros(batch_size)
 
-# Step 5: Save synthetic images
-os.makedirs(new_dir_path, exist_ok=True)
-for i, synthetic_image in enumerate(synthetic_images):
-    image_path = os.path.join(new_dir_path, f"synthetic_image_index_{i}.png")
-    cv2.imwrite(image_path, (synthetic_image * 255).astype(np.uint8))
+        d_loss_real = discriminator.train_on_batch(real_images, real_labels)
+        d_loss_fake = discriminator.train_on_batch(generated_images, fake_labels)
 
-print("Synthetic image generation finished.")
+        d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+        noise = np.random.normal(0, 1, (batch_size, latent_dim))
+        valid_labels = np.ones(batch_size)
+
+        g_loss = gan.train_on_batch(noise, valid_labels)
+
+    if epoch % 100 == 0:
+        print(f"Epoch {epoch}, D Loss: {d_loss}, G Loss: {g_loss}")
+
+        if epoch % 1000 == 0:
+            samples = 10
+            noise = np.random.normal(0, 1, (samples, latent_dim))
+            generated_images = generator.predict(noise)
+            for i in range(samples):
+                plt.imshow(generated_images[i, :, :, 0], cmap='gray')
+                plt.axis('off')
+                plt.savefig(f'{output_dir}/generated_{epoch}_{i}.png')
+                plt.close()
