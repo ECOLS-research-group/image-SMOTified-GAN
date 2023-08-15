ac55d44d97691d24e5ee3c08d7915551263b57bf
Deshant Singh <89639206+deshantDEV29@users.noreply.github.com>
2023-08-15 22:34:34 +1200

    Update in GAN.py

--- GAN.py
+++ GAN.py
@@ -1,141 +1,116 @@
 import os
-from PIL import Image
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torchvision.transforms as transforms
-from torch.utils.data import Dataset, DataLoader
-from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Define the Generator model
+def build_generator(latent_dim, channels):
+    model = models.Sequential()
+    model.add(layers.Dense(128 * 128 * channels, input_dim=latent_dim))
+    model.add(layers.Reshape((128, 128, channels)))
+    model.add(layers.Conv2DTranspose(128, (4, 4), strides=(2, 2), padding='same'))
+    model.add(layers.Conv2DTranspose(64, (4, 4), strides=(2, 2), padding='same'))
+    model.add(layers.Conv2DTranspose(channels, (4, 4), activation='sigmoid', padding='same'))  # Updated here
+    return model
+
+# Define the Discriminator model
+def build_discriminator(img_shape):
+    model = models.Sequential()
+    model.add(layers.Conv2D(64, (4, 4), strides=(2, 2), padding='same', input_shape=img_shape))
+    model.add(layers.Conv2D(128, (4, 4), strides=(2, 2), padding='same'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1, activation='sigmoid'))
+    return model
 
 
-# Set device
-#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-device = torch.device('cpu')
 
-# Hyperparameters
-latent_size = 100
-hidden_size = 256
-image_size = 3 * 64 * 64 # assuming images are RGB and of size 64x64
-batch_size = 64
-num_epochs = 100
-learning_rate = 0.0002
+# Define GAN model
+def build_gan(generator, discriminator):
+    discriminator.trainable = False
+    model = models.Sequential()
 
-# Custom dataset
-class CustomDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
-        self.root_dir = root_dir
-        self.transform = transform
-        self.image_files = [f for f in os.listdir(self.root_dir) if os.path.isfile(os.path.join(self.root_dir, f))]
+    # Add generator layers
+    model.add(generator)
 
-    def __len__(self):
-        return len(self.image_files)
+    # Add discriminator layers (without the fully connected layer)
+    for layer in discriminator.layers[:-1]:  # Exclude the last layer (fully connected)
+        model.add(layer)
 
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
+    return model
 
-        img_name = os.path.join(self.root_dir, self.image_files[idx])
-        image = Image.open(img_name)
+# Parameters
+latent_dim = 100
+img_shape = (128, 128, 3)  # Update this to match your dataset's image size
+channels = img_shape[-1]
+num_categories = 2
 
-        if self.transform:
-            image = self.transform(image)
+# Build and compile the models
+generator = build_generator(latent_dim, channels)
+discriminator = build_discriminator((128, 128, 3))
+discriminator.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(lr=0.0002, beta_1=0.5))  # Compile discriminator
+gan = build_gan(generator, discriminator)
+gan.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(lr=0.0002, beta_1=0.5))
 
-        return image
+# Load and preprocess the dataset using ImageDataGenerator
+datagen = ImageDataGenerator(rescale=1.0 / 255)
 
-# Load custom dataset
-transform = transforms.Compose([
-    transforms.Resize((64, 64)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
-])
-custom_data = CustomDataset(root_dir='./dataset/covid19/train/Covid', transform=transform)
-data_loader = DataLoader(custom_data, batch_size=batch_size, shuffle=True)
+data_dir = 'C:\\Users\\singh\\OneDrive\\Documents\\Projects\\SMOTified-GAN-Image\\data\\xray\\train'
+#data_dir = "C:\\Users\\dsingh\\Documents\\GitHub\\SMOTified-GAN-Image\\data\\xray\\train"
+batch_size = 32  # Adjust the batch size to be a multiple of the number of categories (e.g., 32/2 = 16)
+image_size = (128, 128)
 
-# Discriminator network
-D = nn.Sequential(
-    nn.Linear(image_size, hidden_size),
-    nn.LeakyReLU(0.2),
-    nn.Linear(hidden_size, hidden_size),
-    nn.LeakyReLU(0.2),
-    nn.Linear(hidden_size, 1),
-    nn.Sigmoid()
-).to(device)
+train_generator = datagen.flow_from_directory(
+    data_dir,
+    target_size=image_size,
+    batch_size=batch_size,
+    class_mode='categorical',
+    shuffle=True
+)
 
-# Generator network
-G = nn.Sequential(
-    nn.Linear(latent_size, hidden_size),
-    nn.ReLU(),
-    nn.Linear(hidden_size, hidden_size),
-    nn.ReLU(),
-    nn.Linear(hidden_size, image_size),
-    nn.Tanh()
-).to(device)
 
-# Loss function
-criterion = nn.BCELoss()
+# Create a directory to save generated images
+output_dir = 'C:\\Users\\singh\\OneDrive\\Documents\\Projects\\SMOTified-GAN-Image\\data\\xray\\generated'
+#output_dir = "C:\\Users\\dsingh\\Documents\\GitHub\\SMOTified-GAN-Image\\data\\xray\\generated"
+os.makedirs(output_dir, exist_ok=True)
 
-# Optimizers
-d_optimizer = optim.Adam(D.parameters(), lr=learning_rate)
-g_optimizer = optim.Adam(G.parameters(), lr=learning_rate)
+# Training loop
+epochs = 50
+steps_per_epoch = len(train_generator)
 
-# Tensorboard writer
-writer = SummaryWriter()
+for epoch in range(epochs):
+    for step in range(steps_per_epoch):
+        real_images, real_labels = train_generator.next()  # Get both images and labels from the generator
 
-# Train GAN
-total_step = len(data_loader)
-for epoch in range(num_epochs):
-    for i, (images, _) in enumerate(data_loader):
-        # Adversarial ground truth
-        real_labels = torch.ones(batch_size, 1).to(device)
-        fake_labels = torch.zeros(batch_size, 1).to(device)
+        noise = np.random.normal(0, 1, (batch_size, latent_dim))
+        generated_images = generator.predict(noise)
 
-        # Training discriminator
-        images = images.reshape(batch_size, -1).to(device)
-        outputs = D(images)
-        d_loss_real = criterion(outputs, real_labels)
-        real_score = outputs
+        # Resize generated images to match the discriminator's input shape
+        resized_generated_images = tf.image.resize(generated_images, (128, 128))
 
-        z = torch.randn(batch_size, latent_size).to(device)
-        fake_images = G(z)
-        outputs = D(fake_images.detach())
-        d_loss_fake = criterion(outputs, fake_labels)
-        fake_score = outputs
+        real_labels = np.ones((batch_size, 1))
+        fake_labels = np.zeros((batch_size, 1))
 
-        d_loss = d_loss_real + d_loss_fake
-        d_optimizer.zero_grad()
-        g_optimizer.zero_grad()
-        d_loss.backward()
-        d_optimizer.step()
+        d_loss_real = discriminator.train_on_batch(real_images, real_labels)
+        d_loss_fake = discriminator.train_on_batch(resized_generated_images, fake_labels)
 
-        # Training generator
-        z = torch.randn(batch_size, latent_size).to(device)
-        fake_images = G(z)
-        outputs = D(fake_images)
-        g_loss = criterion(outputs, real_labels)
+        d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
-        d_optimizer.zero_grad()
-        g_optimizer.zero_grad()
-        g_loss.backward()
-        g_optimizer.step()
+        noise = np.random.normal(0, 1, (batch_size, latent_dim))
+        valid_labels = np.ones((batch_size, 1))
 
-        # Print losses
-        if (i+1) % 200 == 0:
-            print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{total_step}], d_loss: {d_loss.item():.4f}, g_loss: {g_loss.item():.4f}')
+        g_loss = gan.train_on_batch(noise, valid_labels)
 
-        # Log losses to Tensorboard
-        writer.add_scalar('Loss/discriminator', d_loss.item(), epoch * total_step + i)
-        writer.add_scalar('Loss/generator', g_loss.item(), epoch * total_step + i)
-        writer.add_scalar('Score/real', real_score.mean().item(), epoch * total_step + i)
-        writer.add_scalar('Score/fake', fake_score.mean().item(), epoch * total_step + i)
+    print(f"Epoch {epoch}, D Loss: {d_loss}, G Loss: {g_loss}")
 
-        # Save generated images to Tensorboard
-        if (i+1) % 1000 == 0:
-            with torch.no_grad():
-                z = torch.randn(batch_size, latent_size).to(device)
-                fake_images = G(z)
-                fake_images = fake_images.reshape(-1, 1, 28, 28)
-                writer.add_images('Generated Images', fake_images, epoch * total_step + i)
+    if epoch % 1 == 0:
+        samples = 10
+        noise = np.random.normal(0, 1, (samples, latent_dim))
+        generated_images = generator.predict(noise)
+        for i in range(samples):
+            plt.imshow(generated_images[i, :, :, 0], cmap='gray')
+            plt.axis('off')
+            plt.savefig(f'{output_dir}/generated_epoch_{epoch}_sample_{i}.png')
+            plt.close()
 
-# Save models
-torch.save(D.state_dict(), 'discriminator.pth')
-torch.save(G.state_dict(), 'generator.pth')
